5c3a3ddf52931837fcc5012a7fc9b4cc785d36a1
TRASALBY <ghtjd8264@naver.com>
2022-09-17 14:54:37 +0900

    1012 유기농 배추

---
+++ 0x09_BFS/6_1012_유기농 배추.kt
@@ -0,0 +1,51 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+private val dx = listOf(0,0,1,-1)
+private val dy = listOf(1,-1,0,0)
+
+fun main() {
+
+
+    val t = readln().toInt()
+    val resultList = ArrayList<Int>()
+    var result = 0
+
+    for (q in 0 until t) {
+        val (m, n, k) = readln().split(" ").map { it.toInt() }
+        val graph = Array(n) { IntArray(m) { 0 } }
+        for (i in 0 until k) {
+            val (x, y) = readln().split(" ").map { it.toInt() }
+            graph[y][x] = 1
+            result = 0
+        }
+        for (i in 0 until n){
+            for (j in 0 until m){
+                if (graph[i][j] == 1) {
+                    val q = LinkedList<Pair<Int, Int>>()
+                    q.add(i to j)
+                    while (q.isNotEmpty()) {
+                        val (x, y) = q.poll()
+
+                        for (k in 0 until 4) {
+                            val nx = x + dx[k]
+                            val ny = y + dy[k]
+
+                            if (0 <= nx && nx < n && 0 <= ny && ny < m && graph[nx][ny] == 1) {
+                                q.add(nx to ny)
+                                graph[nx][ny] = 0
+                            }
+                        }
+                    }
+                    result += 1
+                }
+            }
+        }
+        resultList.add(result)
+    }
+
+    for (answer in resultList){
+        println(answer)
+    }
+
+}
