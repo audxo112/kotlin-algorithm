074be121937bf6d30789a996a605cf9abede2196
TRASALBY <ghtjd8264@naver.com>
2022-09-17 14:55:10 +0900

    7576 토마토 불필요한 과정 생략

--- 0x09_BFS/3_7576_토마토.kt
+++ 0x09_BFS/3_7576_토마토.kt
@@ -1,33 +1,26 @@
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 private val dx = listOf(0,0,1,-1)
 private val dy = listOf(1,-1,0,0)
 private lateinit var graph : Array<IntArray>
-private lateinit var visited : Array<BooleanArray>
 
 fun main(){
     val (m,n) = readln().split(" ").map { it.toInt() }
     graph = Array(n) {IntArray(m) {0} }
-    visited = Array(n) {BooleanArray(m) {false} }
-    val tomatoes = ArrayList<Pair<Int,Int>>()
+    val tomatoes = LinkedList<Pair<Int,Int>>()
 
 
     for (i in 0 until n){
         val line = readln().split(" ").map{it.toInt()}
         line.forEachIndexed{index,it ->
             graph[i][index] = it
-        }
-    }
-
-    for (i in 0 until n){
-        for (j in 0 until m){
-            if (graph[i][j] == 1) {
-                tomatoes.add(j to i)
+            if (it == 1){
+                tomatoes.add(i to index)
             }
         }
     }
+
     bfs(n,m,graph,tomatoes)
 
     var check = 0
@@ -49,22 +42,20 @@ fun main(){
 
 }
 
-private fun bfs(n:Int,m:Int,graph:Array<IntArray>,tomatoes:ArrayList<Pair<Int,Int>>){
+private fun bfs(n:Int,m:Int,graph:Array<IntArray>,tomatoes:LinkedList<Pair<Int,Int>>){
 
-    val q = LinkedList<Pair<Int,Int>>()
-    tomatoes.forEach { q.add(it) }
 
-    while (q.isNotEmpty()){
-        val (x,y) = q.poll()
+    while (tomatoes.isNotEmpty()){
+        val (x,y) = tomatoes.poll()
 
         for (i in 0 until 4){
             val nx = x + dx[i]
             val ny = y + dy[i]
 
-            if(nx < 0 || ny < 0 || nx >= m || ny >= n || graph[ny][nx] == -1) continue
-            else if (graph[ny][nx] == 0){
-                graph[ny][nx] = graph[y][x] + 1
-                q.add(nx to ny)
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] == -1) continue
+            else if (graph[nx][ny] == 0){
+                graph[nx][ny] = graph[x][y] + 1
+                tomatoes.add(nx to ny)
                 continue
             }
             else continue
