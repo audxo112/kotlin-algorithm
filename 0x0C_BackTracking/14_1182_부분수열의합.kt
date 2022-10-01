8af82ada070061e64cc167e208efc866488ff428
Max <>
2022-10-01 16:04:53 +0900

    14_1182 풀이

---
+++ 0x0C_BackTracking/14_1182_부분수열의합.kt
@@ -0,0 +1,44 @@
+import java.util.*
+//https://www.acmicpc.net/problem/1182
+
+private var result = 0
+var output = IntArray(21)
+var used = BooleanArray(21)
+
+fun main() = with(System.`in`.bufferedReader()) {
+
+    var st = StringTokenizer(readLine(), " ")
+    val n = st.nextToken().toInt()
+    val s = st.nextToken().toInt()
+
+    val input = IntArray(n)
+
+    st = StringTokenizer(readLine(), " ")
+    for (i in 0 until n) {
+        input[i] = st.nextToken().toInt()
+    }
+    input.sort()
+
+    for (m in 1..n) {
+        output = IntArray(21)
+        used = BooleanArray(21)
+        back(0, 0, n, m, s, input)
+    }
+
+
+    println(result)
+}
+
+private fun back(depth: Int, i: Int, n: Int, m: Int, s: Int, input: IntArray) {
+    if (depth == m) {
+        if (output.sum() == s) {
+            result++
+        }
+        return
+    }
+
+    for (k in i until n) {
+        output[depth] = input[k]
+        back(depth + 1, k + 1, n, m, s, input)
+    }
+}
