0f56177f103502618a0ef7e7c9ec52108d052ee4
김명석 <audxo112@naver.com>
2022-12-24 23:45:29 +0900

    1644, 1644, 13144: 설명추가 및 성능 개선

--- 0x14_TwoPointer/5_13144_List of Unique Numbers.kt
+++ 0x14_TwoPointer/5_13144_List of Unique Numbers.kt
@@ -3,6 +3,8 @@ package solution13144
 
 import java.io.StreamTokenizer
 
+// 범위 안에 같은 수가 없는 수의 조합을 구해보자
+
 private fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
     fun input(): Int{
         nextToken()
@@ -11,19 +13,23 @@ private fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
 
     val n = input()
     val arr = IntArray(n) {input()}
+    // 존재할 수 있는 수 만큼 미리 배열을 만들어 존재를 확인
     val visited = BooleanArray(100001)
     var count = 0L
 
     var right = 0
     for(left in 0 until n){
-        while(right + 1 <= n && !visited[arr[right]]){
+        // 존재하는 수가 나올때까지 진행
+        while(right < n && !visited[arr[right]]){
             visited[arr[right++]] = true
         }
+        // 만약 끝까지 진행했다면 지금 존재하는 값의 조합을 구함
         if(right >= n){
             val len = right - left
             count += len.toLong() * (len + 1) / 2
             break
         }
+        // left 가 포함된 경우의 수를 더함
         count += right - left
         visited[arr[left]] = false
     }
